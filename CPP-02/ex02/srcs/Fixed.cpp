7c173b964422f40f14c3f5213d81488ee519caa8
asmati <asmati@f1br6s1.paris.42.school>
2026-03-17 15:16:26 +0100

    Automatic update

--- CPP-02/ex02/srcs/Fixed.cpp
+++ CPP-02/ex02/srcs/Fixed.cpp
@@ -6,32 +6,27 @@
 /*   By: asmati <asmati@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 11:00:24 by asmati            #+#    #+#             */
-/*   Updated: 2026/03/17 15:14:18 by asmati           ###   ########.fr       */
+/*   Updated: 2026/03/17 15:16:01 by asmati           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 
 Fixed::Fixed() : 	_value(0)
-{
-	std::cout << "Default constructor called" << std::endl;
-};
+{};
 
 Fixed::Fixed(const Fixed &src)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
 Fixed::Fixed(const int n)
 {
-	std::cout << "Int Constructor Called" << std::endl;
 	this->_value =  (n << _bits);
 }
 
 Fixed::Fixed(const float n)
 {
-	std::cout << "Float Constructor Called" << std::endl;
 	this->_value = roundf(n * (1 << _bits));
 }
 
@@ -66,8 +61,6 @@ Fixed Fixed::operator+(Fixed const & rhs) const {
 
 Fixed & Fixed::operator=(const Fixed &src)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
-	
 	if(this != &src)
 		this->_value = src.getRawBits();
 	return *this;
@@ -95,9 +88,7 @@ const Fixed & Fixed::max(const Fixed & a, const Fixed & b)
 
 
 Fixed::~Fixed()
-{
-	std::cout << "Destructor called" << std::endl;
-};
+{};
 
 int Fixed::toInt(void) const
 {
@@ -106,8 +97,7 @@ int Fixed::toInt(void) const
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
-	return (this->_value);
+		return (this->_value);
 }
 
 void Fixed::setRawBits( int const raw )
